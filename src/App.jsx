058347d4ca58a7272056be6d4c67bcab362a4036
Alexis Antonio Arano Castañeda <l18350291@gmail.com>
2022-03-29 09:42:39 -0600

    Contreol de los state (campos de texto)

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,20 @@ import { useState } from "react";
 
 function App() {
   //Hooks
-  const [tituloState, setTituloState] = useState("");
-  const [fechaState, setFechaState] = useState("");
-  const [notaState, setNotaState] = useState("");
+  const [inputState, setInputState] = useState({
+    titulo: "",
+    fecha: "",
+    nota: "",
+  });
 
-  const handleChangeTitulo = (event) => {
-    setTituloState(event.target.value);
 
-  }
+  const handleInputChange = (event) => {
+    setInputState({
+      ...inputState,
+      [event.target.name]: event.target.value,
+    });
 
-  const handleChangeFecha = (event) => {
-    setFechaState(event.target.value);
-
-  }
-
-  const handleChangeNota = (event) => {
-    setNotaState(event.target.value);
-
-  }
+  };
   
   return (
     <div className="App">
@@ -29,8 +25,8 @@ function App() {
         id = "titulo" 
         name = "titulo" 
         type = "text" 
-        onChange = {handleChangeTitulo}
-        value = {tituloState}
+        onChange = {handleInputChange}
+        value = {inputState.titulo}
       />
       <br></br>
       <label htmlfor = "fecha">Fecha</label>
@@ -38,8 +34,8 @@ function App() {
         id = "fecha" 
         name = "fecha" 
         type = "text" 
-        onChange = {handleChangeFecha}
-        value = {fechaState}
+        onChange = {handleInputChange}
+        value = {inputState.fecha}
       />
       <br></br>
       <label htmlfor = "nota">Nota</label>
@@ -47,8 +43,8 @@ function App() {
         id = "nota" 
         name = "nota" 
         type = "text" 
-        onChange = {handleChangeNota}
-        value = {notaState}
+        onChange = {handleInputChange}
+        value = {inputState.nota}
       />
 
     </div>
